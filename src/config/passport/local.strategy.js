94089e1847ff2e0ef47775b1c523d6a430257d98
wilder78 <173951485+wilder78@users.noreply.github.com>
2025-04-12 13:26:57 -0500

    Decodificación del login clase 4.

--- src/config/passport/local.strategy.js
+++ src/config/passport/local.strategy.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { userDao } from "../../persistence/mongo/dao/user.dao.js";
-import { hasPassword } from "../../utils/hasPassword.utils.js";
+import { comparePassword, hasPassword } from "../../utils/hasPassword.utils.js";
 
 // Estrategia de registro
 const registerStrategy = new Strategy(
@@ -10,8 +10,8 @@ const registerStrategy = new Strategy(
     try {
       const user = await userDao.getOne({ email: username });
       if (user) return done(null, false, { message: "El usuario ya existe." });
-        // console.log(req.body);
-        
+      // console.log(req.body);
+
       // Si el usuario no esta registrado, procedemos a registrarlo.
       const newUser = {
         ...req.body,
@@ -21,9 +21,8 @@ const registerStrategy = new Strategy(
       const userCreate = await userDao.create(newUser);
 
       return done(null, userCreate);
-
     } catch (error) {
-        done(error);
+      done(error);
     }
   }
 );
@@ -32,19 +31,37 @@ const registerStrategy = new Strategy(
 passport.use("register", registerStrategy);
 
 // Estrategia de login
+const loginStrategy = new Strategy(
+  { usernameField: "email" },
+  async (username, password, done) => {
+    try {
+      
+      const user = await userDao.getOne({ email: username });
+      if(!user || !comparePassword(user.password, password)) return done(null, false, { message: "email o password no válidos."});
+      
+      // En caso de que las credenciales esten bien.
+      return done(null, user);
+
+    } catch (error) {
+      done(error)
+    }
+  }
+)
+
+//  Registramos la estrategia de login en passport.
+passport.use("login", loginStrategy);
 
 // Serialización --//-- Serialization.
 passport.serializeUser((user, done) => {
-    done(null, user._id);
-})
+  done(null, user._id);
+});
 
 // Deserializar --//-- Deserialize.
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await userDao.getOne({ _id: id});
-        done(null, user);
-
-    } catch (error) {
-        done(error)
-    }
-})
+  try {
+    const user = await userDao.getOne({ _id: id });
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -10,98 +10,55 @@ import passport from "passport";
 const router = Router();
 
 // Metodo para validar usuario o logearse.
-router.post("/login", async (req, res) => {
+router.post("/login", passport.authenticate("login"), async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await userDao.getOne({ email });
-    // console.log(await comparePassword(user.password, password));
-    
-    if (!user || !comparePassword(user.password, password)) 
-      return res.status(401).json({ message: "Email o password invalido" });
-
-    // Crear un token
-    const tokenInfo = {
-      _id: user._id,
-      email: user.email,
-      role: user.role
-    }
-
-    const token = createToken(tokenInfo);
-
-    // Guardemos el token en una cookie
-    res.cookie("token", token, { httpOnly: true});
-
-    res.status(200).json({user, token});
+    res.status(200).json({ user: req.user });
   } catch (error) {
-    console.error("Error en /login:", error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: "error", message: "Internal Server Error." });
   }
 });
-
 
 // Metodo para registrar usuarios.
-router.post("/register", passport.authenticate("register"), async(req, res) => {
-  try {
-
-    res.status(201).json({ message: req.user});
-
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
+router.post(
+  "/register",
+  passport.authenticate("register"),
+  async (req, res) => {
+    try {
+      res.status(201).json({ message: req.user });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "error", message: "Internal Server Error" });
+    }
   }
-});
-// router.post("/register", async (req, res) => {
-//   try {
-//     // console.log("Cuerpo recibido en /register:", req.body); //
-//     const { email, password } = req.body;
-
-//     // Verificar que el password fue enviado
-//     if (!password) {
-//       return res.status(400).json({ message: "El password es obligatorio" });
-//     }
-
-//     // Verificar si el usuario ya existe
-//     const user = await userDao.getOne({ email });
-//     if (user) {
-//       return res.status(400).json({ message: "Ya hay un usuario registrado con ese email" });
-//     }
-
-//     // Hashear el password
-//     const newUserData = {
-//       ...req.body,
-//       password: hasPassword(password),
-//     };
-
-//     // Crear un nuevo usuario
-//     const newUser = await userDao.create(newUserData);
-
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.error("Error en /register:", error);
-//     res.status(500).json({ status: "error", message: "Internal Server Error" });
-//   }
-// });
-
+);
 
 // Verificar que el usuario está autenticado con JWT
-router.get("/profile", checkTokenCookie, authRole(["admin", "user"]),  async (req, res) => {
-  try {
-
-    res.status(200).json({ user: req.user });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "error", message: "Internal Server Error" });
+router.get(
+  "/profile",
+  checkTokenCookie,
+  authRole(["admin", "user"]),
+  async (req, res) => {
+    try {
+      res.status(200).json({ user: req.user });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ status: "error", message: "Internal Server Error" });
+    }
   }
-});
-
+);
 
 // Cerrar login.
 router.get("/logout", async (req, res) => {
   try {
     req.session.destroy();
-    res.status(200).json({ message: "Session cerrada"});
+    res.status(200).json({ message: "Session cerrada" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: "error", message: "Internal Server Error" });
